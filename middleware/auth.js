f81b5b0e99aa8a632dcbe9635225f12faf444a4f
Abba Emmanuel <eabba21@gmail.com>
2024-07-25 19:14:31 +0100

    updated the main.js file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,25 @@
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const authenticationMiddleware = async (req, res, next) => {
-  console.log(req.headers.authorization);
-  next();
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+ 
+    // @ts-ignore
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { id, username } = decoded;
+    req.user = { id, username };
+    next();
+  } catch (error) {
+    throw new CustomAPIError("Not allowed to access this route");
+  }
 };
 
 module.exports = authenticationMiddleware;
